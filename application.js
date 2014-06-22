b5cc8a283f8b7257101ac21ee928032d08dcb5c5
Howard Mao <zhehao.mao@gmail.com>
2014-06-21 17:56:57 -0700

    first commit

---
+++ application.js
@@ -0,0 +1,11 @@
+/*
+ * This is the main javascript file. The code for your Tic-Tac-Toe program
+ * should go in here.
+ */
+
+function setup() {
+    // Put any setup code (code that only runs in the very beginning) here.
+    console.log("Hello, World!");
+}
+
+$(document).ready(setup);
